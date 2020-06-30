add4ff0a359f159c5bf159e27181789e2714d5d3
firgo12 <60897048+firgo12@users.noreply.github.com>
2020-06-30 18:55:50 -0300

    Add files via upload

--- Jogo da Memória 2.0/jogo-da-memoria.js
+++ Jogo da Memória 2.0/jogo-da-memoria.js
@@ -1,6 +1,10 @@
-    function startGame(){
+        var acertos = 0;
+        
         var images = [];
 
+        var cartasviradas = [];
+
+        //Estrutura de repetição que inicializa o jogo fazendo a distribuição das cartas dentro da "mesa"
         for(var i = 0; i < 16; i++){
             var img = {
                 src: "img/" + i + ".png",
@@ -8,7 +12,15 @@
             };
             images.push(img);
         }
+        startGame();
 
+        function startGame(){
+        //Embaralhar as cartas
+        cartasviradas = [];
+        images = randomsort(images);
+
+        var rostoFrente = document.getElementsByClassName("frente");
+        //Vai colocar as cartas em uma fileira com id = 0 até id = 7
         for(var i = 0; i < 16; i++){
             var carta = document.querySelector("#carta" + i);
             console.log(carta);
@@ -17,11 +29,63 @@
 
             carta.addEventListener("click", flipCard, false);
 
+            rostoFrente[i].style.background = "url('"+ images[i].src +"')"
+            rostoFrente[i].setAttribute("id",images[i].id)
+
         }
     }
-    function flipCard(){
-        var rostos = this.getElementsByClassName("rosto");
-        rostos[0].classList.toggle("virado");
-        rostos[1].classList.toggle("virado");
+    //Embaralhar as cartas (função)
+    function randomsort(oldArray){
+        var newArray = [];
+        while(newArray.length != oldArray.length){
+            var i = Math.floor(Math.random()*oldArray.length);
+
+            if(newArray.indexOf(oldArray[i]) < 0){
+                newArray.push(oldArray[i]);
+            }
+
+        }
+        return newArray;
+
     }
-startGame();
+
+    //Girar as cartas + a implementação do terceiro clique para desvirar as cartas
+    function flipCard(){
+        if(cartasviradas.length < 2){
+            var rostos = this.getElementsByClassName("rosto");
+
+            if(rostos[0].classList.length > 2){
+                return;
+            }
+            rostos[0].classList.toggle("virado");
+            rostos[1].classList.toggle("virado");
+
+            cartasviradas.push(this);
+
+            if(cartasviradas.length == 2){
+                if(cartasviradas[0].childNodes[3].id == cartasviradas[1].childNodes[3].id){
+                    cartasviradas[0].childNodes[1].classList.toggle("acertos");
+                    cartasviradas[0].childNodes[3].classList.toggle("acertos");
+                    cartasviradas[1].childNodes[1].classList.toggle("acertos");
+                    cartasviradas[1].childNodes[3].classList.toggle("acertos");
+
+                    acertos ++;
+
+                    cartasviradas = [];
+
+                    if(acertos == 8){
+                        
+                    }
+                }
+            }
+        }
+        
+        else{
+            cartasviradas[0].childNodes[1].classList.toggle("virado");
+            cartasviradas[0].childNodes[3].classList.toggle("virado");
+            cartasviradas[1].childNodes[1].classList.toggle("virado");
+            cartasviradas[1].childNodes[3].classList.toggle("virado");
+
+            cartasviradas = [];
+        }
+    }
